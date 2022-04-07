c35a964a763b4b8cac646fbe74d6e879b5847eaa
kkreddyk <kkreddy.k@gmail.com>
2022-04-07 21:05:16 +0100

    Added comments on how to handle reading files from src/main/resource

--- spark32/src/main/scala/spark32/testingResource.scala
+++ spark32/src/main/scala/spark32/testingResource.scala
@@ -8,13 +8,13 @@ object testingResource {
     
     println("Starting "+this.getClass.getName)
     
-    val fs=this.getClass().getClassLoader().getResourceAsStream("log4j.properties")
+    val fs=this.getClass().getClassLoader().getResourceAsStream("log4j.properties") //To read the file from src/main/resources into BufferedInputStream
+    println("Data Type of fs="+fs.getClass.getSimpleName) //To get data type of the variable use var.getClass.getSimpleName
+
+    val dataString=scala.io.Source.fromInputStream(fs).mkString //To read from BufferedInputStream
+    println("Data Type of dataString="+dataString.getClass.getSimpleName)
     
-    val dataString=scala.io.Source.fromInputStream(fs).mkString
-    
-    println(dataString.getClass.getSimpleName)
-    
-    println(dataString)
+    println("Printing content of the file::"+dataString)
 
 
     println("Ending "+this.getClass.getName)
