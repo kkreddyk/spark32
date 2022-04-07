d382b86cb0a1057e32dbc877f83b22e4a0b1daf2
kkreddyk <kkreddy.k@gmail.com>
2022-04-07 21:07:53 +0100

    CTRL+I for Indentation

--- spark32/src/main/scala/spark32/stream1.scala
+++ spark32/src/main/scala/spark32/stream1.scala
@@ -13,42 +13,42 @@ import java.util.Date
 import org.apache.spark.sql.streaming
 
 object stream1 {
-  
-  def main(args: Array[String]){
-    
-    println("Starting Streaming::::::::")
-    
-    val logger: Logger = LogManager.getLogger(this.getClass.getName)
-    
-    println("Log Level"+logger.getEffectiveLevel)
-    
-    
-    val dateFormat: SimpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
-    System.setProperty("runid", dateFormat.format(new Date()));
-    
-    
-    PropertyConfigurator.configure(args(0))
-    
-    logger.info("INFO-Logger")
-    logger.error("ERROR-Logger")
-    logger.debug("DEBUG-Logger")
-    val spark=SparkSession.builder.master("local[*]").getOrCreate
-    
-    spark.conf.getAll.foreach(println)
-    
-    
-   val streamDF1=spark.readStream.format("socket").
-   option("host","192.168.28.131").option("port","9090").load()
-   
-   streamDF1.printSchema()
-   println("Steaming?? = "+streamDF1.isStreaming)
-   
-   streamDF1.createOrReplaceTempView("streamdf1")
-   
-   spark.sql("select * from streamdf1").writeStream.format("console").outputMode("append")
-   
-   spark.sql("select value,count(*) from streamdf1 group by value").writeStream.format("console").outputMode("complete").start().awaitTermination()
-   //streamDF1.writeStream.format("console").start().awaitTermination()
-  }
-  
+
+	def main(args: Array[String]){
+
+		println("Starting Streaming::::::::")
+
+		val logger: Logger = LogManager.getLogger(this.getClass.getName)
+
+		println("Log Level"+logger.getEffectiveLevel)
+
+
+		val dateFormat: SimpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+		System.setProperty("runid", dateFormat.format(new Date()));
+
+
+		PropertyConfigurator.configure(args(0))
+
+		logger.info("INFO-Logger")
+		logger.error("ERROR-Logger")
+		logger.debug("DEBUG-Logger")
+		val spark=SparkSession.builder.master("local[*]").getOrCreate
+
+		spark.conf.getAll.foreach(println)
+
+
+		val streamDF1=spark.readStream.format("socket").
+		option("host","192.168.28.131").option("port","9090").load()
+
+		streamDF1.printSchema()
+		println("Steaming?? = "+streamDF1.isStreaming)
+
+		streamDF1.createOrReplaceTempView("streamdf1")
+
+		spark.sql("select * from streamdf1").writeStream.format("console").outputMode("append")
+
+		spark.sql("select value,count(*) from streamdf1 group by value").writeStream.format("console").outputMode("complete").start().awaitTermination()
+		//streamDF1.writeStream.format("console").start().awaitTermination()
+	}
+
 }
--- spark32/src/main/scala/spark32/testingResource.scala
+++ spark32/src/main/scala/spark32/testingResource.scala
@@ -1,22 +1,22 @@
 package spark32
 
 object testingResource {
-  
-  
-  def main(args: Array[String])
-  {
-    
-    println("Starting "+this.getClass.getName)
-    
-    val fs=this.getClass().getClassLoader().getResourceAsStream("log4j.properties") //To read the file from src/main/resources into BufferedInputStream
-    println("Data Type of fs="+fs.getClass.getSimpleName) //To get data type of the variable use var.getClass.getSimpleName
-
-    val dataString=scala.io.Source.fromInputStream(fs).mkString //To read from BufferedInputStream
-    println("Data Type of dataString="+dataString.getClass.getSimpleName)
-    
-    println("Printing content of the file::"+dataString)
 
 
-    println("Ending "+this.getClass.getName)
-  }
+	def main(args: Array[String])
+	{
+
+		println("Starting "+this.getClass.getName)
+
+		val fs=this.getClass().getClassLoader().getResourceAsStream("log4j.properties") //To read the file from src/main/resources into BufferedInputStream
+		println("Data Type of fs="+fs.getClass.getSimpleName) //To get data type of the variable use var.getClass.getSimpleName
+
+		val dataString=scala.io.Source.fromInputStream(fs).mkString //To read from BufferedInputStream
+		println("Data Type of dataString="+dataString.getClass.getSimpleName)
+
+		println("Printing content of the file::"+dataString)
+
+
+		println("Ending "+this.getClass.getName)
+	}
 }
