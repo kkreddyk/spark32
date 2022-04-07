3422d394546601cb0df76609fee5f15ddcdf9ecf
kkreddyk <kkreddy.k@gmail.com>
2022-04-07 21:07:24 +0100

    Indentation using CTRL+I

--- spark32/src/main/scala/spark32/jdbc.scala
+++ spark32/src/main/scala/spark32/jdbc.scala
@@ -13,57 +13,57 @@ import org.apache.log4j.PropertyConfigurator
 import org.apache.log4j.Level
 
 object jdbc {
-  
-  
- def main(args: Array[String]){
-   
-   println("JDBC Start::")
-   
-  
-   
- val logger: Logger = LogManager.getLogger(this.getClass.getName)
-    
-     val dateFormat: SimpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
-    System.setProperty("runid", dateFormat.format(new Date()));
-    
-    PropertyConfigurator.configure(args(0))
-     logger.info("Logger : Welcome to log4j::"+this.getClass.getName)
-    
-      val spark=SparkSession.builder.master("local[*]").getOrCreate()
-   
-   spark.sparkContext.setLogLevel("ERROR")
-   spark.conf.getAll.foreach(println)
-   
-val jdbcDF = spark.read
-  .format("jdbc")
-.option("url", "jdbc:postgresql://localhost:5432/postgres")
-.option("user", "postgres")
-.option("password", "postgres")
-.option("dbtable","public.t1")
-.load()
-  
-  jdbcDF.show(false)
-   
-  
-  val connectionProperties = new Properties()
-connectionProperties.put("user", "postgres")
-connectionProperties.put("password", "postgres")
 
-val query="(select count(*) as cnt from public.t1 )q"
 
-//val jdbcDF2 = spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres","public.t1",connectionProperties)
- 
-val jdbcDF2 = spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres",query,connectionProperties)
- 
+	def main(args: Array[String]){
 
-//Thread.sleep(100000)
-jdbcDF2.show(false)
+		println("JDBC Start::")
 
-val target_table="public.t2"
 
-jdbcDF2.write.mode("append").option("truncate","true").jdbc("jdbc:postgresql://localhost:5432/postgres",target_table,connectionProperties)
 
-spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres","public.t2",connectionProperties).show(false)
- 
- }
+		val logger: Logger = LogManager.getLogger(this.getClass.getName)
+
+		val dateFormat: SimpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+		System.setProperty("runid", dateFormat.format(new Date()));
+
+		PropertyConfigurator.configure(args(0))
+		logger.info("Logger : Welcome to log4j::"+this.getClass.getName)
+
+		val spark=SparkSession.builder.master("local[*]").getOrCreate()
+
+		spark.sparkContext.setLogLevel("ERROR")
+		spark.conf.getAll.foreach(println)
+
+		val jdbcDF = spark.read
+		.format("jdbc")
+		.option("url", "jdbc:postgresql://localhost:5432/postgres")
+		.option("user", "postgres")
+		.option("password", "postgres")
+		.option("dbtable","public.t1")
+		.load()
+
+		jdbcDF.show(false)
+
+
+		val connectionProperties = new Properties()
+		connectionProperties.put("user", "postgres")
+		connectionProperties.put("password", "postgres")
+
+		val query="(select count(*) as cnt from public.t1 )q"
+
+		//val jdbcDF2 = spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres","public.t1",connectionProperties)
+
+		val jdbcDF2 = spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres",query,connectionProperties)
+
+
+		//Thread.sleep(100000)
+		jdbcDF2.show(false)
+
+		val target_table="public.t2"
+
+		jdbcDF2.write.mode("append").option("truncate","true").jdbc("jdbc:postgresql://localhost:5432/postgres",target_table,connectionProperties)
+
+		spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres","public.t2",connectionProperties).show(false)
+
+	}
 }
--- spark32/src/main/scala/spark32/log4jworking.scala
+++ spark32/src/main/scala/spark32/log4jworking.scala
@@ -11,81 +11,81 @@ import java.util.Date
 import org.apache.spark.sql.SparkSession
 
 object log4jworking {
-  
-  
-  def main(args: Array[String]){
-    
-    println("Testing log4j")
-    
-     //val spark=SparkSession.builder().master("local[*]").getOrCreate()
-	 //spark.conf.getAll.foreach(println)
-    val logger: Logger = LogManager.getLogger(this.getClass.getName)
-    
-     val dateFormat: SimpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
-    System.setProperty("log4j.current.date.time", dateFormat.format(new Date()));
-    
-    PropertyConfigurator.configure(args(0))
-     logger.info("Logger : Welcome to log4j::"+this.getClass.getName)
 
-     //logger.setLevel(Level.DEBUG)
-     //logger.setLevel(Level.INFO)
-    
-	  logger.debug("This is debug message");
-	  logger.info("This is info message");
-	  logger.warn("This is warn message");
-	  logger.fatal("This is fatal message");
-	  logger.error("This is error message");
-	    logger.error("======");
-	    
-	    logger.debug("This is debug message");
-	  logger.info("This is info message");
-	  logger.warn("This is warn message");
-	  logger.fatal("This is fatal message");
-	  logger.error("This is error message");
-	  
-	  logger.debug("This is debug message");
-	  logger.info("This is info message");
-	  logger.warn("This is warn message");
-	  logger.fatal("This is fatal message");
-	  logger.error("This is error message");
-	  
-	  logger.debug("This is debug message");
-	  logger.info("This is info message");
-	  logger.warn("This is warn message");
-	  logger.fatal("This is fatal message");
-	  logger.error("This is error message");
-	  
-	  logger.debug("This is debug message");
-	  logger.info("This is info message");
-	  logger.warn("This is warn message");
-	  logger.fatal("This is fatal message");
-	  logger.error("This is error message");
-	  
-	  logger.debug("This is debug message");
-	  logger.info("This is info message");
-	  logger.warn("This is warn message");
-	  logger.fatal("This is fatal message");
-	  logger.error("This is error message");
-	  
 
-	  println("log level:::::"+ logger.getEffectiveLevel)
-	  
+	def main(args: Array[String]){
 
-	  Logger.getLogger("org").setLevel(Level.OFF)
-    
-	  Logger.getLogger("akka").setLevel(Level.OFF)
-	  
-	  logger.info("Testing logger INFO")
-	  
-	   logger.info("----------"+Logger.getRootLogger)
-	   
-	  val spark=SparkSession.builder().master("local[*]").getOrCreate()
-	  
-	  
-	    //spark.sparkContext.setLogLevel("DEBUG")
+		println("Testing log4j")
 
-	  //spark.sql("select * from a").show(false)
-	  spark.conf.getAll.foreach(println)
-	  
-  }
+		//val spark=SparkSession.builder().master("local[*]").getOrCreate()
+		//spark.conf.getAll.foreach(println)
+		val logger: Logger = LogManager.getLogger(this.getClass.getName)
+
+		val dateFormat: SimpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+		System.setProperty("log4j.current.date.time", dateFormat.format(new Date()));
+
+		PropertyConfigurator.configure(args(0))
+		logger.info("Logger : Welcome to log4j::"+this.getClass.getName)
+
+		//logger.setLevel(Level.DEBUG)
+		//logger.setLevel(Level.INFO)
+
+		logger.debug("This is debug message");
+		logger.info("This is info message");
+		logger.warn("This is warn message");
+		logger.fatal("This is fatal message");
+		logger.error("This is error message");
+		logger.error("======");
+
+		logger.debug("This is debug message");
+		logger.info("This is info message");
+		logger.warn("This is warn message");
+		logger.fatal("This is fatal message");
+		logger.error("This is error message");
+
+		logger.debug("This is debug message");
+		logger.info("This is info message");
+		logger.warn("This is warn message");
+		logger.fatal("This is fatal message");
+		logger.error("This is error message");
+
+		logger.debug("This is debug message");
+		logger.info("This is info message");
+		logger.warn("This is warn message");
+		logger.fatal("This is fatal message");
+		logger.error("This is error message");
+
+		logger.debug("This is debug message");
+		logger.info("This is info message");
+		logger.warn("This is warn message");
+		logger.fatal("This is fatal message");
+		logger.error("This is error message");
+
+		logger.debug("This is debug message");
+		logger.info("This is info message");
+		logger.warn("This is warn message");
+		logger.fatal("This is fatal message");
+		logger.error("This is error message");
+
+
+		println("log level:::::"+ logger.getEffectiveLevel)
+
+
+		Logger.getLogger("org").setLevel(Level.OFF)
+
+		Logger.getLogger("akka").setLevel(Level.OFF)
+
+		logger.info("Testing logger INFO")
+
+		logger.info("----------"+Logger.getRootLogger)
+
+		val spark=SparkSession.builder().master("local[*]").getOrCreate()
+
+
+		//spark.sparkContext.setLogLevel("DEBUG")
+
+		//spark.sql("select * from a").show(false)
+		spark.conf.getAll.foreach(println)
+
+	}
 }
