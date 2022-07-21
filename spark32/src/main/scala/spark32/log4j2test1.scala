41923ecfe7acc9eb8c26dcb8871fccab4471a902
kkreddyk <kkreddy.k@gmail.com>
2022-07-21 20:50:12 +0100

    ...32

---
+++ spark32/src/main/scala/spark32/log4j2test1.scala
@@ -0,0 +1,18 @@
+package spark32
+
+import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.LogManager
+
+object log4j2test1 {
+  
+  def main(args: Array[String])
+  {
+  val logger: Logger = LogManager.getLogger(this.getClass.getName)
+  
+  logger.debug("Log4j2: Debug....")
+  logger.error("Log4j2: error")
+  logger.warn("Log4j2: warn")
+  logger.fatal("Log4j2: fatal")
+         
+  }
+}
